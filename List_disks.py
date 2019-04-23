aeb03f530f616b0082edf91f175228443bd286f9
fabricedupre <32126510+duprefm@users.noreply.github.com>
2019-04-23 17:05:13 +0200

    Fonctionnel
    Fonctionnel sur des projets pour lesquels  j'ai les droits de lister les ressources.

--- List_disks.py
+++ List_disks.py
@@ -30,22 +30,29 @@ serviceprojects = discovery.build('cloudresourcemanager', 'v1', credentials=cred
 # Project ID for this request.
 project = 'p-gcp-cloudcostanalysis'  # TODO: Update placeholder value.
 
+global liste_projects
+liste_projects = []
+
 # The name of the zone for this request.
 #zone = 'europe-west1-b'  # TODO: Update placeholder value.
 
 # Lists Projects that are visible to the user
 def list_projects():
-    #serviceprojects = discovery.build('cloudresourcemanager', 'v1', credentials=credentials)
-    response = serviceprojects.projects().list().execute()
+    tags_filter = '(labels.cost_center_number:*)'
+    response = serviceprojects.projects().list(filter=tags_filter).execute()
     for project in response.get('projects', []):
         # TODO: Change code below to process each `project` resource:
-        projectId = project['projectId']
-        print(projectId)
-        
+        project = project['projectId']
+        #pprint(project)
+        liste_projects.append(project)
+        #print(liste_projects)
     
   
 # List of Zone resources available to the specified project
 def list_zones(project):
+    r = service.zones().list(project=project)
+    print(r.text)
+    #print(r.status_code)
     result = service.zones().list(project=project).execute()
     if 'items' in result:
         return result['items']
@@ -61,31 +68,39 @@ def list_disks(zone):
     else:
         return []
 #Main
-#list_projects()
-
 list_projects()
 
-for zones in list_zones(project):
-    #pprint(zone)
-    zone_name = zones['name']
-    #print(zone_name)
-    for disks in list_disks(zone_name):
-        #Get disks with detach time
-        if 'lastDetachTimestamp' in disks:
-            last_detach_time = disks['lastDetachTimestamp']
-            date_last_detach_time = datetime.fromisoformat(last_detach_time)
-            tz_date_last_detach_time = date_last_detach_time.replace(tzinfo=timezone.utc).timestamp()
-            #Get disk size
-            sizeGb = disks['sizeGb']
-            #Calculate delta in days       
-            now = datetime.now()
-            tz_now = now.replace(tzinfo=timezone.utc).timestamp()
-            delta = (tz_now - tz_date_last_detach_time)/86400             
-            #Get disk name
-            disk_name = disks['name']
-            #Outputs
-            #print(delta)
-            #pprint(disks)
-            print("%s;%s;%s;%.0f;%s" % (project, zone_name, disk_name, delta, sizeGb))
-#zones = list_zones(project)
-#pprint(zones)
+#liste_projects = ['p-gcp-cloudcostanalysis', 'padawan-p2']
+#liste_projects = ['p-vrd-lai-farmstar-crop-monit']
+for project in liste_projects:
+    #print(project)
+    for zones in list_zones(project):
+        #print(zones)
+        zone_name = zones['name']
+        for disks in list_disks(zone_name):
+            #Get disks with detach time
+            if 'lastDetachTimestamp' in disks:
+                last_detach_time = disks['lastDetachTimestamp']
+                date_last_detach_time = datetime.fromisoformat(last_detach_time)
+                tz_date_last_detach_time = date_last_detach_time.replace(tzinfo=timezone.utc).timestamp()
+                #Get disk size
+                sizeGb = 0
+                sizeGb = disks['sizeGb']
+                #Calculate delta in days and months      
+                now = datetime.now()
+                tz_now = now.replace(tzinfo=timezone.utc).timestamp()
+                delta_days = (tz_now - tz_date_last_detach_time)/86400
+                delta_months = (tz_now - tz_date_last_detach_time)/(86400*30)
+                #Get disk name
+                disk_name = disks['name']
+                #Get disk type
+                disk_type_str = disks['type'].split("/")
+                disk_type = disk_type_str[-1]
+                print(disk_type)
+                #Calculate cost of unattached disks
+                if disk_type == 'pd-standard':
+                    prize = delta_months * 0.04 * int(sizeGb)
+                if disk_type == 'pd-ssd':
+                    prize = delta_months * 0.17 * int(sizeGb)
+                #Outputs
+                print("%s;%s;%s;%.0f days;%.0f months;%s Gb;$ %.0f" % (project, zone_name, disk_name, delta_days, delta_months, sizeGb, prize))
